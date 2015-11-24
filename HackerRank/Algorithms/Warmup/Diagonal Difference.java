d9aa77bf5984a67da35e72a4ace43f01ac005516
Pham Nguyen Nhat Trung <el.desperado.381@gmail.com>
2015-11-24 14:35:09 +0700

    Added some HackerRank solutions

---
+++ HackerRank/Algorithms/Warmup/Diagonal Difference.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a[][] = new int[n][n];
+        for(int a_i=0; a_i < n; a_i++){
+            for(int a_j=0; a_j < n; a_j++){
+                a[a_i][a_j] = in.nextInt();
+            }
+        }
+        
+        int primaryDiagonalLength = getDiagonalLength(a, n, true);
+        int optionalDiagonalLength = getDiagonalLength(a, n, false);
+
+        int difference = Math.abs(primaryDiagonalLength - optionalDiagonalLength);
+        //Print Result
+        System.out.println(difference);
+    }
+    
+    static int getDiagonalLength(int[][] matrix, int matrixSize, boolean isPrimaryDiagonal) {
+        int column = 0, row = 0, length = 0;
+        if (!isPrimaryDiagonal) {
+            row = matrixSize - 1;
+        }
+
+        do {
+            int matrixPoint = matrix[row][column];
+            length += matrixPoint;
+            
+            // Increase or decrease index
+            if (isPrimaryDiagonal) {
+                row++;
+                if(row >= matrixSize) {
+                    break;
+                }
+            } else {
+                row--;
+                if(row < 0) {
+                    break;
+                }
+            }
+            column++;
+
+            if (column >= matrixSize) {
+                break;
+            }
+        } while(1==1);
+        
+        return length;
+    }
+}
---
+++ HackerRank/Algorithms/Warmup/Simple Array Sum.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for(int arr_i=0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();
+        }
+        int sum = sumOfNumberArray(arr);
+        System.out.println(sum);
+    }
+    
+    static int sumOfNumberArray(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+}
