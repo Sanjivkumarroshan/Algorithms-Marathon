e328c9bcef654ec6abf7c5660d8a2bc404ebfefa
Pham Nguyen Nhat Trung <el.desperado.381@gmail.com>
2015-11-24 14:40:26 +0700

    Added Questions' Description

--- HackerRank/Algorithms/Warmup/Diagonal Difference.java
+++ HackerRank/Algorithms/Warmup/Diagonal Difference.java
@@ -1,3 +1,17 @@
+/*
+Problem Statement
+
+Given a square matrix of size N×N, calculate the absolute difference between the sums of its diagonals.
+
+Input Format
+
+The first line contains a single integer, N. The next N lines denote the matrix's rows, with each line containing N space-separated integers describing the columns.
+
+Output Format
+
+Print the absolute difference between the two sums of the matrix's diagonals as a single integer.
+*/
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
